877373b20adf011ff3c5039a0f4fde95cae02b55
Moshe Weiner <moshesweiner@gmail.com>
2022-02-08 09:30:43 +0200

    remove darkmode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import {useSelector, useDispatch} from 'react-redux'
 import {increment, decrement } from './redux/counter'
-import {toggleDarkMode} from './redux/theme'
 
 
 function App() {
   const count = useSelector(state => state.counter.count)
-  const count = {isDarkMode}(state => state.counter.count)
   const dispatch = useDispatch()
   const isDarkMode = true
   
@@ -22,9 +20,7 @@ function App() {
          style={{backgroundColor: "red"}}  
          className="App-buttons-button">Decrement</button>
 
-         <button onClick={() => {dispatch(isDarkMode())}}
-         style={{backgroundColor: "blue"}}  
-         className="App-buttons-button">Dark Mode</button>
+        
         
         
 
